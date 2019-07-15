74b62d084d2769e21f6722c8a6530e7591fde42e
mauriciomac92 <mauricio.macias@csu.fullerton.edu>
2019-07-15 16:48:48 -0700

    added Preloader.js

---
+++ A&M Games/Preloader.js
@@ -0,0 +1,14 @@
+A_MGames.Preloader = function(game){
+this.preloadBar = null;
+this.titleText = null;
+this.ready = false;
+};
+
+A_MGames.Preloader.prototype = {
+
+    preload: function() {
+
+        
+    }
+
+}
